62888812d98ab0bb0d92ed0c65d6667518ec1556
Mohammad Reza <en.salemi@outlook.com>
2022-11-11 13:09:11 +0330

    update: dynamic header title

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Header from "./header";
 
-const Layout = () => {
+const Layout = ({ headerTitle }: { headerTitle: string }) => {
   return (
     <div className="flex flex-col min-h-screen bg-[#E5E5E5] font-yekan">
-      <Header />
+      <Header title={headerTitle} />
 
       <main className="px-8 mb-4">
         <Outlet />
