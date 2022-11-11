8b59a741f34fbc8683e64ef2796f80bfaeeccbc8
Mohammad Reza <en.salemi@outlook.com>
2022-11-11 19:40:28 +0330

    update: render message for empty question list

--- src/pages/question-list.tsx
+++ src/pages/question-list.tsx
@@ -16,20 +16,24 @@ const QuestionList = () => {
 
   return (
     <>
-      {questions
-        ? questions.map((question) => (
-            <QuestionCard
-              type="question"
-              key={question.id}
-              id={question.id}
-              title={question.title}
-              text={question.text}
-              time={question.time}
-              date={question.date}
-              commentsCount={question.commentsCount}
-            />
-          ))
-        : null}
+      {questions.length ? (
+        questions.map((question) => (
+          <QuestionCard
+            type="question"
+            key={question.id}
+            id={question.id}
+            title={question.title}
+            text={question.text}
+            time={question.time}
+            date={question.date}
+            commentsCount={question.commentsCount}
+          />
+        ))
+      ) : (
+        <p className="flex mt-20 justify-center text-4xl text-green-600">
+          هنوز هیچ سوالی ثبت نشده
+        </p>
+      )}
     </>
   );
 };
